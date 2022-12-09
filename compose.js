6d252ce04515f5a12713c1d1d49073b59b64d3f8
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2022-12-09 12:41:19 +0100

    Update compose.js

--- compose.js
+++ compose.js
@@ -1,1 +1,24 @@
+/**
+ * Compose une fonction à partir de deux.
+ * forme : fnA.fnB / fnB.fnA
+ * 
+ * @param {String} forme
+ * @param {String} fnA 
+ * @param {String} fnB 
+ * @returns 
+ */
+function compose(forme, fnA, fnB) {
+    
 
+    return "0";
+}
+
+
+/**
+ * Transforme une fonction composé en tableau de fonction.
+ * @param {String} fn 
+ * @returns 
+ */
+function decompose(fn) {
+    return [];
+}
