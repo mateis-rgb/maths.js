085c990d66458909022fcab5ef4ff0dcefc05cd9
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2022-12-09 12:55:41 +0100

    Update compose.js

--- compose.js
+++ compose.js
@@ -8,14 +8,29 @@
  * @returns 
  */
 function compose(forme, fnA, fnB) {
-    
+    let struct = forme.split(".");
 
-    return "0";
+    if (struct[0] == "fnA" && struct[1] == "fnB") {
+        struct[0] = fnA;
+        struct[1] = fnB;
+    }
+    else if (struct[0] == "fnB" && struct[1] == "fnA") {
+        struct[0] = fnB;
+        struct[1] = fnA;
+    }
+
+    let fonction = struct[0].replace("x", `(${struct[1]})`);
+
+    return fonction;
 }
+
+console.log(compose("fnA.fnB", "x^2-2", "e^x"));
 
 
 /**
  * Transforme une fonction composé en tableau de fonction.
+ * fn est de forme
+ * 
  * @param {String} fn 
  * @returns 
  */
